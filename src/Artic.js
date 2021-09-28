1eed6206abedbbc87b3fc9d2bf7bc03eb4b9e99c
hv <87605749+harsha-bandi@users.noreply.github.com>
2021-09-28 23:23:02 +0530

    Add files via upload

---
+++ src/Artic.js
@@ -0,0 +1,11 @@
+import react,{Component} from 'react';
+class Artic extends Component{
+    render(){
+        return(
+            <div>
+                <h1>it's my frst react prgm</h1>
+            </div>
+        )
+    }
+}
+export default Artic;
---
+++ src/Product.js
@@ -0,0 +1,11 @@
+import react,{Component} from 'react';
+class Product extends Component{
+    render(){
+        return(
+            <div>
+                <h1>learning react JS</h1>
+            </div>
+        )
+    }
+}
+export default Product;
